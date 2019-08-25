3bb09debac6ec7903c14fa9875a4501da19eede3
yjs3694 <yjs3694@gmail.com>
2019-08-25 15:41:31 +0900

    Merge

--- AbotCrawler/Program.cs
+++ AbotCrawler/Program.cs
@@ -17,10 +17,10 @@ namespace AbotCrawler
                 // 옵션과 함께 크롤러 인스턴스 생성할 경우
                 var crawlConfig = new CrawlConfiguration();
                 crawlConfig.CrawlTimeoutSeconds = 5000;
-                //                crawlConfig.MaxConcurrentThreads = 10;
+//                crawlConfig.MaxConcurrentThreads = 10;
 
                 crawlConfig.MaxConcurrentThreads = 1;
-                //                crawlConfig.MaxPagesToCrawl = 10;
+//                crawlConfig.MaxPagesToCrawl = 10;
                 crawlConfig.MaxPagesToCrawl = 50;
                 crawlConfig.UserAgentString = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0";
                 IWebCrawler crawler = new PoliteWebCrawler(crawlConfig);
@@ -38,8 +38,8 @@ namespace AbotCrawler
 
                     string fn = pg.Uri.Segments[pg.Uri.Segments.Length - 1];
                     string path = @"C:\Users\yjs3694\source\repos\AbotCrawler\AbotCrawler\bin\Debug\crawl.txt";
-                    //                    File.WriteAllText(fn, pg.Content.Text);
-                    //                    File.WriteAllText(directory, pg.Content.Text);
+//                    File.WriteAllText(fn, pg.Content.Text);
+//                    File.WriteAllText(directory, pg.Content.Text);
 
                     doc.LoadHtml(pg.Content.Text);
 
@@ -52,26 +52,26 @@ namespace AbotCrawler
                     //singleNode 노드의 프로퍼티(클래스)값을 리턴한다.
                     //  string className = singleNode.GetAttributeValue("class", "");
 
-                    //                    HtmlAgilityPack.HtmlNodeCollection article = doc.DocumentNode.SelectNodes("//div[@class='articles']");
-                    //                    HtmlAgilityPack.HtmlNodeCollection article = doc.DocumentNode.SelectNodes("div[@class='hotissue_builtin']");
+//                    HtmlAgilityPack.HtmlNodeCollection article = doc.DocumentNode.SelectNodes("//div[@class='articles']");
+//                    HtmlAgilityPack.HtmlNodeCollection article = doc.DocumentNode.SelectNodes("div[@class='hotissue_builtin']");
 
 
                     if (singleNode != null)
                     {
-                        //                        File.WriteAllText(directory, singleNode.SelectSingleNode(".//article/div[1]/div[0]/div[2]").InnerText);
+//                        File.WriteAllText(directory, singleNode.SelectSingleNode(".//article/div[1]/div[0]/div[2]").InnerText);
 
                         //*[@id="mArticle"]/div[2]/div[1]/div[2]/div[1]/ol
                         //*[@id="mArticle"]/div[2]/div[1]/div[3]/div[1]/ol
 
-                        //                        var content = singleNode.SelectSingleNode("//div[2]/div[1]/div[2]/div[1]/ol")?.InnerText;
+//                        var content = singleNode.SelectSingleNode("//div[2]/div[1]/div[2]/div[1]/ol")?.InnerText;
                         var content = singleNode.SelectSingleNode("//div[2]/div[1]/div[3]/div[1]/ol")?.InnerText;
 
                         if (content != null)
                         {
                             var bbb = content.Replace("\n\n\n", "");
 
-                            //                            File.WriteAllText(path, bbb);
-                            //                            File.AppendAllText(path, "\n\n\n");
+//                            File.WriteAllText(path, bbb);
+//                            File.AppendAllText(path, "\n\n\n");
                             File.AppendAllText(path, bbb);
                             File.AppendAllText(path, "\n\n\n");
                         }
